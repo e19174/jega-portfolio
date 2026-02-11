f04b0d3ef852604b6f23c6e3e7724c3a7117cda6
Jegatheesan <115539955+e19174@users.noreply.github.com>
2026-02-11 11:16:51 +0530

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/jega-portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
@@ -25,3 +24,4 @@ const App = () => (
 );
 
 export default App;
+
